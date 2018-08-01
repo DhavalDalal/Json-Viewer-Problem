c81ff0b45cb822eefd482355c4b445f837ef3d7d
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2018-08-01 11:19:54 +0530

    Added Structure for Java9 and pre Java9 builds

---
+++ src/main/java/com/tsys/viewer/Main.java
@@ -0,0 +1,7 @@
+package com.tsys.viewer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Json Viewer - Lets Begin Implementing!");
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.tsys.viewer {
+  exports com.tsys.viewer;
+  
+  requires json;
+  requires JCDP;
+  requires commons.cli;
+}
